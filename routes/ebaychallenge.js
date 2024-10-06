b85ea3e402a705aa6b127e8097097cd39afe6f01
Samuel Egemba <egembasam@gmail.com>
2024-10-06 19:57:22 +0100

    challengeResponse

--- routes/ebaychallenge.js
+++ routes/ebaychallenge.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+
 
 /* GET home page. */
-router.get('/challenge', function(req, res, next) {
-    var challengeCode =req.query('challenge_code');
+router.get('/', function(req, res, next) {
+    var challengeCode =req.query.challenge_code;
     var hostname = req.get('host');
     var endpoint = hostname+'/challenge';
+    console.log(endpoint)
     var verificationToken = 'jg65287het545263t56423_6t74h83rhjkbsd56_'
-    const hash = createHash('sha256');
+    const hash = crypto.createHash('sha256');
     hash.update(challengeCode);
     hash.update(verificationToken);
     hash.update(endpoint);
     const responseHash = hash.digest('hex');
+    res.header({'Content-Type': 'application/json'})
+    res.json({
+        'challengeResponse': responseHash.toString()
+    })
 console.log(new Buffer.from(responseHash).toString());
 });
+   
 
 module.exports = router;
 
