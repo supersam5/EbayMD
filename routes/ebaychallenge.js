84d80f7eb996e37e48a774baccb1fe489f95f830
Samuel Egemba <egembasam@gmail.com>
2024-10-06 21:01:00 +0100

    implemented post method

--- routes/ebaychallenge.js
+++ routes/ebaychallenge.js
@@ -18,10 +18,28 @@ router.get('/', function(req, res, next) {
     const responseHash = hash.digest('hex');
     res.header({'Content-Type': 'application/json'})
     res.status(200).json({
-        'challengeResponse': responseHash.toString()
+        "challengeResponse": responseHash.toString()
     })
 console.log(new Buffer.from(responseHash).toString());
 });
+router.post('/', function(req, res, next) {
+    var challengeCode =req.query.challenge_code;
+    var hostname = req.get('host');
+    var endpoint = hostname+'/challenge';
+    console.log(challengeCode)
+    console.log(endpoint)
+    var verificationToken = 'jg65287heEt545263t56423_6t7JKh83rhjkbsd-56'
+    const hash = crypto.createHash('sha256');
+    hash.update(challengeCode);
+    hash.update(verificationToken);
+    hash.update(endpoint);
+    const responseHash = hash.digest('hex');
+    res.header({'Content-Type': 'application/json'})
+    res.status(200).json({
+        "challengeResponse": responseHash.toString()
+    })
+console.log(new Buffer.from(responseHash).toString());
+})
    
 
 module.exports = router;
