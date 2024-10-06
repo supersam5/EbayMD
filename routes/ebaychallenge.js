35c0e3a49a2e50980953839aabd08139bdbbae03
Samuel Egemba <egembasam@gmail.com>
2024-10-06 20:45:52 +0100

    updaed token

--- routes/ebaychallenge.js
+++ routes/ebaychallenge.js
@@ -8,15 +8,16 @@ router.get('/', function(req, res, next) {
     var challengeCode =req.query.challenge_code;
     var hostname = req.get('host');
     var endpoint = hostname+'/challenge';
+    console.log(challengeCode)
     console.log(endpoint)
-    var verificationToken = 'jg65287het545263t56423_6t74h83rhjkbsd56_'
+    var verificationToken = 'jg65287heEt545263t56423_6t7JKh83rhjkbsd-56'
     const hash = crypto.createHash('sha256');
     hash.update(challengeCode);
     hash.update(verificationToken);
     hash.update(endpoint);
     const responseHash = hash.digest('hex');
     res.header({'Content-Type': 'application/json'})
-    res.json({
+    res.status(200).json({
         'challengeResponse': responseHash.toString()
     })
 console.log(new Buffer.from(responseHash).toString());
